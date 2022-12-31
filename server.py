46dab2c807c0eff8450bcffec2734a3b175ecbbc
R-Udayakumar <udayar2004@gmail.com>
2022-12-31 19:13:42 +0530

    success

--- server.py
+++ server.py
@@ -1,35 +1,4 @@
-# Developing a Simple Webserver
-
-# AIM:
-
-Develop a webserver to display about top five web application development frameworks.
-
-# DESIGN STEPS:
-
-## Step 1:
-
-HTML content creation is done
-
-## Step 2:
-
-Design of webserver workflow
-
-## Step 3:
-
-Implementation using Python code
-
-## Step 4:
-
-Serving the HTML pages.
-
-## Step 5:
-
-Testing the webserver
-
-# PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
 content = """
 <!DOCTYPE html>
 <html>
@@ -59,11 +28,3 @@ server_address = ('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-```
-
-# OUTPUT:
-![OUTPUT](./images/output.png)
-
-# RESULT:
-
-The program is executed succesfully
